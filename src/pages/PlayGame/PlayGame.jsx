f988107d7b688b7c836b30c17bbc472ef278b19d
abhijeet-hms-work <abhijeet.hms.work@gmail.com>
2024-12-21 11:13:37 +0530

    added  button

--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,13 +1,24 @@
 import { Link } from "react-router-dom";
 import MaskedText from "../../Components/MakedText/MaskedText.jsx";
 import LetterButtons from "../../Components/LetterButtons/LetterButtons.jsx";
+import { useState } from "react";
 const PlayGame = () => {
+  const [usedLetters, setUsedLetters] = useState([]);
+
+  const handleLetterClick = function (letter) {
+    setUsedLetters([...usedLetters, letter]);
+  };
+
   return (
     <div>
       <h1>PlayGame</h1>
-      <MaskedText text="humble" usedLetters={["b", "e"]} />
+      <MaskedText text="humble" usedLetters={usedLetters} />
       <hr />
-      <LetterButtons usedLetters={["b", "e"]} />
+      <LetterButtons
+        text="humble"
+        usedLetters={usedLetters}
+        onLetterClick={handleLetterClick}
+      />
 
       <hr />
       <Link to="/start"> Start Game</Link>
