0544c54df81c2a1c9caa9856332400dd5aba8f47
abhijeet-hms-work <abhijeet.hms.work@gmail.com>
2024-12-21 10:50:39 +0530

    added letter button

--- src/Components/MakedText/MaskedText.jsx
+++ src/Components/MakedText/MaskedText.jsx
@@ -19,3 +19,5 @@ function MaskedText({ text, usedLetters }) {
     </>
   );
 }
+
+export default MaskedText;
--- src/pages/PlayGame/PlayGame.jsx
+++ src/pages/PlayGame/PlayGame.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
-
+import MaskedText from "../../Components/MakedText/MaskedText.jsx";
+import LetterButtons from "../../Components/LetterButtons/LetterButtons.jsx";
 const PlayGame = () => {
   return (
     <div>
-      PlayGame
+      <h1>PlayGame</h1>
+      <MaskedText text="humble" usedLetters={["b", "e"]} />
+      <hr />
+      <LetterButtons usedLetters={["b", "e"]} />
+
+      <hr />
       <Link to="/start"> Start Game</Link>
     </div>
   );
